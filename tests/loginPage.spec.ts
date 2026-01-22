4ba2f98ac7e9e861e2b33c6cf82fb977a148bb63
OlgaRaukuts <raukuts.olga@gmail.com>
2026-01-22 12:27:50 +0100

    added a simple test to see if the framework works

--- tests/loginPage.spec.ts
+++ tests/loginPage.spec.ts
@@ -1,7 +1,10 @@
-import {test} from '@playwright/test';
+import {test, expect} from '@playwright/test';
 
 test.describe('Login Page Tests', () => {
     test.beforeEach(async ({page}) => {
         await page.goto('https://automationteststore.com/index.php?rt=account/login');
     })
+    test('should load login page', async ({ page }) => {
+        await expect(page).toHaveTitle(/Account Login/);
+    });
 })
