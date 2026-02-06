45c7613824d7f99742cb55f1d684f8b1f7ce7441
OlgaRaukuts <raukuts.olga@gmail.com>
2026-02-06 10:53:50 +0100

    adding multiple products to a cart

--- tests/cartPage.spec.ts
+++ tests/cartPage.spec.ts
@@ -9,13 +9,13 @@ test.describe('Cart Page Tests', () => {
         await expect(page).toHaveURL(/automationteststore\.com/);
     });
 
-    test('should add product to cart -> open cart and verify product exists', async ({ page }) => {
+    test('should add a product to cart -> open cart and verify product exists', async ({ page }) => {
         await page.locator('#featured').locator('a.productcart[data-id="50"]').click();
         await page.locator('li[data-id="menu_cart"] > a:visible').click();
         await expect(page.locator('input[name="quantity[50]"]')).toBeVisible();
-    })
+    });
 
-    test('should add product to cart -> quick basket exists', async ({ page }) => {
+    test('should add a product to cart -> quick basket exists', async ({ page }) => {
         await page.locator('#featured a.productcart[data-id="50"]').click();
         const productCard = page.locator('#featured div.pricetag.jumbotron', {
       has: page.locator('a.productcart[data-id="50"]'), });
@@ -24,5 +24,14 @@ test.describe('Cart Page Tests', () => {
         const quickBasket = productCard.locator('.quick_basket');
         await expect(quickBasket).toBeVisible();
         await expect(quickBasket.locator('a[title="Added to cart"]')).toBeVisible();
-});
+    });
+
+        test('should add multiple products to cart -> open cart and verify product exists', async ({ page }) => {
+        await page.locator('#featured').locator('a.productcart[data-id="50"]').click();
+        await page.locator('#featured').locator('a.productcart[data-id="51"]').click();
+        await page.locator('li[data-id="menu_cart"] > a:visible').click();
+        await expect(page.locator('input[name="quantity[50]"]')).toBeVisible();
+        await expect(page.locator('input[name="quantity[51]"]')).toBeVisible();
+    });
+
 });
