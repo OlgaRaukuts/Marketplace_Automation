08917d5041ce6695af29e68f5b0d2adac9cb5df7
OlgaRaukuts <raukuts.olga@gmail.com>
2026-02-06 10:52:06 +0100

    another test for adding a product to the cart

--- tests/cartPage.spec.ts
+++ tests/cartPage.spec.ts
@@ -2,7 +2,7 @@ import {test, expect} from '@playwright/test';
 
 test.describe('Cart Page Tests', () => {
     test.beforeEach(async ({ page }) => {
-        await page.goto('https://automationteststore.com');
+        await page.goto('https://automationteststore.com',{waitUntil: 'domcontentloaded',});
     });
 
     test('should load products page', async ({ page }) => {
@@ -11,7 +11,18 @@ test.describe('Cart Page Tests', () => {
 
     test('should add product to cart -> open cart and verify product exists', async ({ page }) => {
         await page.locator('#featured').locator('a.productcart[data-id="50"]').click();
-        await page.getByRole('link', { name: 'Shopping Cart' }).click();
-        await expect(page.getByText('Product Name:')).toBeVisible();
+        await page.locator('li[data-id="menu_cart"] > a:visible').click();
+        await expect(page.locator('input[name="quantity[50]"]')).toBeVisible();
     })
+
+    test('should add product to cart -> quick basket exists', async ({ page }) => {
+        await page.locator('#featured a.productcart[data-id="50"]').click();
+        const productCard = page.locator('#featured div.pricetag.jumbotron', {
+      has: page.locator('a.productcart[data-id="50"]'), });
+        await expect(productCard).toBeVisible();
+        await expect(productCard).toHaveClass(/added_to_cart/);
+        const quickBasket = productCard.locator('.quick_basket');
+        await expect(quickBasket).toBeVisible();
+        await expect(quickBasket.locator('a[title="Added to cart"]')).toBeVisible();
+});
 });
